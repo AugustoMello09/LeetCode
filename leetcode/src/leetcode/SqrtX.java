bf36579e59cc994654ff422885e220ed24eb4d48
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2025-03-14 15:51:47 -0300

    feat : 69. Sqrt(x)

---
+++ leetcode/src/leetcode/SqrtX.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class SqrtX {
+
+	public static void main(String[] args) {
+		System.out.println(mySqrt(8));
+	}
+	
+	public static int mySqrt(int x) {
+		int numero = (int) Math.sqrt(x);
+		return numero;
+	}
+}
